373d18efa88597eb2e90c91581d307566142367e
Stéphane Rochoy <stephane.rochoy@stormshield.eu>
2018-03-02 15:07:46 +0100

    Removed go-away. Code cleanup.

--- layers/core/funcs.el
+++ layers/core/funcs.el
@@ -2,46 +2,47 @@
   "Display a message but clearly identify it as coming from one of Sheda's layers."
   (message (concat "(sheda) " format-string) args))
 
-
-(defun sheda-core/switch-to-messages-buffer ()
-  "Switch to the *Messages* buffer."
-  (interactive)
-  (switch-to-buffer "*Messages*"))
-
-(defun sheda-core/switch-to-reminders-buffer ()
-  "Switch to the reminders buffer."
-  (interactive)
-  (let* ((buffer (get-buffer "dot.reminders")))
+(defun sheda-core/switch-to-file-buffer (basename dirname)
+  "Switch to the buffer visiting the file BASENAME located in the directory DIRNAME, i.e., DIRNAME/BASENAME."
+  (let* ((buffer (get-buffer basename)))
     (if (null buffer)
-        (find-file "~/ens/conf/dot.reminders")
-      (switch-to-buffer buffer))))
-
-(defun sheda-core/switch-to-work-org-buffer ()
-  "Switch to the work.org buffer."
-  (interactive)
-  (let* ((buffer (get-buffer "work.org")))
-    (if (null buffer)
-        (find-file "~/ens/org/work.org")
+        (find-file (concat dirname "/" basename))
       (switch-to-buffer buffer))))
 
 (defun sheda-core/run-push ()
   "Run the push command."
   (start-process "push" "*Push Log*" (expand-file-name "bin/push" user-home-directory)))
 
-(defun sheda-core/go-away ()
-  "Go away from keyboard."
-  (interactive)
-  (sheda-core/message "Locking session...")
-  ;; XXX Should test for existence before using the jabber-* functions.
-  (jabber-send-presence "away" "I'm away from keyboard right now" 0)
-  (async-start-process "lock-session"
-                       (expand-file-name "bin/lock-session" user-home-directory)
-                       (lambda (process)
-                         (jabber-send-presence "" "Ready to chat" 0)
-                         (sheda-core/message "Session unlocked."))))
-
 (defun sheda-core/adjust-keys-for-helm (map)
   (define-key map (kbd "C-c") 'helm-previous-source)
   (define-key map (kbd "C-t") 'helm-next-line)
   (define-key map (kbd "C-s") 'helm-previous-line)
   (define-key map (kbd "C-r") 'helm-next-source))
+
+;; SWITCHING TO BUFFERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun sheda-core/switch-to-messages-buffer ()
+  "Switch to the *Messages* buffer."
+  (interactive)
+  (switch-to-buffer "*Messages*"))
+
+
+(defun sheda-core/switch-to-reminders-buffer ()
+  "Switch to the reminders buffer."
+  (interactive)
+  (sheda-core/switch-to-file-buffer "dot.reminders" "~/ens/conf"))
+
+(defun sheda-core/switch-to-work-org-buffer ()
+  "Switch to the work.org buffer."
+  (interactive)
+  (sheda-core/switch-to-file-buffer "work.org" "~/ens/org"))
+
+(defun sheda-core/switch-to-tac-org-buffer ()
+  "Switch to the tac.org buffer."
+  (interactive)
+  (sheda-core/switch-to-file-buffer "tac.org" "~/ens/org"))
+
+(defun sheda-core/switch-to-qa-org-buffer ()
+  "Switch to the qa.org buffer."
+  (interactive)
+  (sheda-core/switch-to-file-buffer "qa.org" "~/ens/org"))
--- layers/core/keybindings.el
+++ layers/core/keybindings.el
@@ -1,6 +1,8 @@
 (spacemacs/set-leader-keys
-  "oa" 'sheda-core/go-away
+  "RET" 'beacon-blink
   "oe" 'sheda-core/switch-to-messages-buffer
   "or" 'sheda-core/switch-to-reminders-buffer
+  "ot" 'sheda-core/switch-to-tac-org-buffer
   "ow" 'sheda-core/switch-to-work-org-buffer
+  "oq" 'sheda-core/switch-to-qa-org-buffer
   )
