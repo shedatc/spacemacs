1514ec7a6d099bf196cb6a986c6ab105793c3988
Stéphane Rochoy <stephane.rochoy@stormshield.eu>
2019-02-19 08:33:26 +0100

    Stop playing with the @Org layout for now.

--- layers/org/funcs.el
+++ layers/org/funcs.el
@@ -21,3 +21,22 @@
 (defun sheda-org/branch-url (branch)
   "Return the URL to view the given BRANCH. Note that BRANCH is double-encoded."
   (format "https://review-sns.stormshield.eu/source/firmware/history/%s/" (url-hexify-string (url-hexify-string branch))))
+
+(defun sheda-org/add-current-buffer-to-org-layout ()
+  "Add the current buffer to the @Org layout."
+  (let* ((b (current-buffer))
+         (p (persp-get-by-name "@Org")))
+    (when p
+      (sheda-core/message "Adding buffer %S to layout %S..." b p)
+      (persp-add-buffer b p))))
+
+(defun sheda-org/add-all-org-buffers-to-org-layout ()
+  "Look for any Org buffer and add it to the @Org layout."
+  (let* ((p (persp-get-by-name "@Org")))
+    (when p (mapcar (lambda (b)
+                      "If buffer is an Org buffer, add it to the @Org layout."
+                      (with-current-buffer b
+                        (when (and (string= major-mode "org-mode")
+                                   (not (persp-contain-buffer-p b p)))
+                          (persp-add-buffer b p))))
+                    (buffer-list)))))
