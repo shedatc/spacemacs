692bb0c18c1c383883375df06e5f521b77dd1cee
Stéphane Rochoy <stephane.rochoy@stormshield.eu>
2019-03-14 16:42:02 +0100

    WIP: Try to sign mails by default.

--- layers/communication/config.el
+++ layers/communication/config.el
@@ -3,7 +3,9 @@
       message-send-mail-function 'message-send-mail-with-sendmail
       send-mail-function         'sendmail-send-it
 
-      ;; XXX
+      ;; XXX OpenPGP
+      mml-secure-openpgp-signers          '("ACDB01A8142DC48A") ;;  Stéphane Rochoy <stephane.rochoy@stormshield.eu>
+      mml-secure-openpgp-encrypt-to-self  t
       mml-secure-openpgp-sign-with-sender t
 
       ;; DEBUG
